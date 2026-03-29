2a503c9e8453fc6d6ebeefbc6c0b14a91febe985
Ahsan-Mudassar <ahsanmudassar441@gmail.com>
2026-03-29 21:07:47 +0500

    add css file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
 
-const container = document.getElementById('root');
 
-if (container) {
-  const root = ReactDOM.createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-} else {
-  console.error("Failed to find the root element");
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
